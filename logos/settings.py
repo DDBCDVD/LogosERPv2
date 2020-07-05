4f1c550142e7a97469e2b64e1200be78bc2e69d0
DDBCDVD <ddbcdvd@gmail.com>
2020-07-05 14:19:52 -0400

    Vistas y validaciones adiciones Usuarios

--- logos/settings.py
+++ logos/settings.py
@@ -149,3 +149,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 MEDIA_URL = '/media/'
 SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "logos.softech@gmail.com"
+EMAIL_HOST_PASSWORD = "united117*"
+
+# from django.core.mail import send_mail
+
+# send_mail("prueba_asunto", "Mensaje", "logos.softech@gmail.com", ["ddbcdvd@gmail.com"], fail_silently=False)
